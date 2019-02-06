2925ade03b325fefcdf004d3acc497139bce924c
DavidFainstein <47358966+DavidFainstein@users.noreply.github.com>
2019-02-06 12:22:16 -0300

    6/2 completado
    commit completo

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,20 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	//· PRIMER COMANDO DE CONSOLA    ·/
+
+	var import1;
+	var descuento;
+	var valor;
+
+	import1=importe.value;
+	import1=parseInt(import1);
+
+	valor=import1*10/100;
+
+	descuento=import1-valor;
+
+    resultado.value=descuento;
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,21 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var import1;
+	var aumento;
+	var valor;
+
+	import1=sueldo.value;
+	import1=parseInt(import1);
+
+	valor=import1*10/100;
+
+	aumento=(valor+import1);
+
+    resultado.value=aumento;
+
+
+
 
 	
 }
