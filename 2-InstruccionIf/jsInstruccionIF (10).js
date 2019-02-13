2f6affa3829e03a992edaaac0f04e2545235f8ba
alumno <alumno>
2019-02-13 12:23:35 -0300

    ejercicios if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,31 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numerorandom;
+	var mensaje;
+
+	numerorandom=Math.floor((Math.random() * 10) + 1);
+	mensaje="";
+
+	//alert(numerorandom);
+	console.log(numerorandom);
+
+	 if (numerorandom>=9)
+	 {
+	 	mensaje="EXCELENTE"
+	 	else
+	 	{
+	 		if(numerorandom>4)
+	 		{
+	 			mensaje="APROBADO"
+	 			else
+	 			{
+	 				mensaje="Vamos, la proxima se puede"
+	 			}
+	 		}
+	 	}
+	 }
 	
+
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,51 @@
 function mostrar()
 {
+	var producto1;
+	var preciodelproducto;
+	var descuento;
+	var descadd;
+	var metododepago;
+	var preciofinal;
+	var recargo;
+	var mensaje;
+
+	producto1=prompt("ingrese la cantidad de productos que desea comprar","");
+	producto1=parseInt(producto1);
+	preciodelproducto=prompt("ingrese el valor del producto","");
+	preciodelproducto=parseInt(preciodelproducto);
+
+	mensaje="precio a pagar es de $";
+
+	if(producto1>2)
+	{ 
+		descuento=preciodelproducto*10/100;
+		preciofinal=preciofinal-descuento;
+
+	 else(preciodelproducto>1999) 
+	 {  
+
+	 	descadd=preciodelproducto*15/100;
+		preciofinal=preciofinal-descadd;
+	 }	
+
+	  alert(mensaje+preciofinal);
+
+	}	
+		metododepago=prompt("ingrese metodo de pago, ya sea tarjeta o efectivo","");
+
+		if(metododepago=="tarjeta")
+		{	
+
+			recargo=preciofinal*10/100;
+			preciofinal=preciofinal+recargo;
+
+			alert(mensaje+preciofinal);
+
+
+		}	
+
+
+
+
 
 }
