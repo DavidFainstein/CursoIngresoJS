4148f780ea673b436644b9d1ac0beada40789444
alumno <alumno>
2019-02-13 09:34:17 -0300

    ejercicio 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
-  
+	var precio;
+	var recargo;
+	var metododepago;
+	var preciofinal;
+
+	precio=elNombre.value;
+	precio=parseInt(precio);
+	metododepago=laLocalidad.value;
+	recargo=parseInt(recargo);
+
+	recargo=precio*10/100;
+
+	preciofinal=precio+recargo;
+
+	alert("el precio del producto en efectivo es: $"+precio+", con tarjeta tiene un recargo del 10% que seria "+recargo+" de recargo, costandole final $"+preciofinal);
+   
 }
