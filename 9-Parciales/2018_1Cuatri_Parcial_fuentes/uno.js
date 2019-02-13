a5e84ce95e193536f2d5f148a52641629bea1ed2
alumno <alumno>
2019-02-13 09:13:13 -0300

    ejercicio 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,24 @@
 
 function mostrar()
 {
+	var nombre1;
+	var nombre2;
+	var edad1;
+	var edad2;
+	var suma;
 
+	nombre1=prompt("Ustedes son","");
+	nombre2=prompt("y","");
+	edad1=prompt("su edad es ","");
+	edad2=prompt("y","");
+
+	edad1=parseInt(edad1);
+	edad2=parseInt(edad2);
+
+	suma=edad1+edad2;
+
+	alert("Ustedes son "+nombre1+" y "+nombre2+" sus edades son "+edad1+" y "+edad2+" y la suma de sus edades es "+suma);
+
+
+  
 }
